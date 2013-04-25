adedd410c3a64f893d1d57c1f4fab00cda3bc153
Ian & Brian <Ian & Brian>
2013-04-24 18:37:02 -0700

    done for the day, in the middle of implementing un-faving

--- app/assets/javascripts/views/qist_detail_view.js
+++ app/assets/javascripts/views/qist_detail_view.js
@@ -1,6 +1,7 @@
 QS.Views.QistDetailView = Backbone.View.extend({
 	events: {
-		"click button#favorite" : "favorite"
+		"click button#favorite" : "favorite",
+		"click button#unfavorite": "unFavorite"
 	},
 
 	render: function() {
@@ -15,9 +16,35 @@ QS.Views.QistDetailView = Backbone.View.extend({
 		return that;
 	},
 
+	toggleButton: function() {
+		var that = this;
+		var $button = $(".fav");
+
+		if ( $button.prop("id") == "favorite" ) {
+			$button.prop("id", "unfavorite").text("UnFav!!");
+		} else {
+			$button.prop("id", "favorite").text("Fav!!");
+		}
+	},
+
+	unFavorite: function() {
+		console.log("hey whatthehell");
+		var that = this;
+		QS.Models.Favorite.bind('remove', function() {
+			this.destroy();
+		});
+		that.toggleButton();
+		var fav = that.model.get("favorites")
+							.findWhere({user_id: QS.Store.currentUserId});
+
+		console.log(fav);
+		//that.model.get("favorites").remove({}, { qist: that.model });
+	},
 
 	favorite: function() {
 		var that = this;
+		that.toggleButton();
+
 		that.model.get("favorites").create({}, { qist: that.model });
 	}
 });
